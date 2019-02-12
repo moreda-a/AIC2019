bfb97482a0c6e48caf3e2278087b340b1a6455ec
mohammadreza <daneshvaramoli@ce.sharif.edu>
2019-02-13 01:27:02 +0330

    movement

--- AIC19/src/code/Movement.java
+++ AIC19/src/code/Movement.java
@@ -1,12 +1,18 @@
 package code;
 
-import client.model.World;
+import java.util.Random;
+
+import client.model.*;
 
 public class Movement extends Util {
 
 	public static void moveTurn(World world) {
-		// TODO Auto-generated method stub
+		Random random = new Random();
+		Hero[] heroes = world.getMyHeroes();
 
+		for (Hero hero : heroes) {
+			world.moveHero(hero, Direction.values()[random.nextInt(4)]);
+		}
 	}
 
 }
